2be0ea6fee73fdc476875f95abb5e24fdec15436
obawunmidara <obawunmidara@gmail.com>
2025-08-14 13:49:37 +0100

    new changes

--- hotel.js
+++ hotel.js
@@ -1,16 +1,3 @@
-function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ['Year', 'Visitations', { role: 'style' } ],
-        ['2010', 10, 'color: gray'],
-        ['2020', 14, 'color: #76A7FA'],
-        ['2030', 16, 'opacity: 0.2'],
-        ['2040', 22, 'stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF'],
-        ['2050', 28, 'stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2']
-      ]);
-    }
-
-
-
 function toggleDropdown() {
     var dropdownList = document.getElementById("dropdownList");
     if (dropdownList.style.display === "none" || dropdownList.style.display === "") {
@@ -28,30 +15,12 @@ function toggleDropdown() {
         dropdownContainer.style.display = "none";
     }
 }
-google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawMultSeries);
-
-function drawMultSeries() {
-      var data = google.visualization.arrayToDataTable([
-        ['New York City, NY', 8175000, 8008000],
-        ['Los Angeles, CA', 3792000, 3694000],
-        ['Chicago, IL', 2695000, 2896000],
-        ['Houston, TX', 2099000, 1953000],
-        ['Philadelphia, PA', 1526000, 1517000]
-      ]);
-
-      var options = {
-        title: 'Population of Largest U.S. Cities',
-        chartArea: {width: '50%'},
-        hAxis: {
-          title: 'Total Population',
-          minValue: 0
-        },
-        vAxis: {
-          title: 'City'
-        }
-      };
-
-      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
+function all(){
+    var dropdownLis = document.getElementById("all-type");
+    if (dropdownLis.style.display === "none" || dropdownLis.style.display === "") {
+        dropdownLis.style.display = "block";
+    } else {
+        dropdownLis.style.display = "none";
     }
+
+}
